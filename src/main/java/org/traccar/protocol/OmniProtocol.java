826da69e08bcbba3b20b1e4910d314a8aa65a71d
Hussein <shm379@live.com>
2026-07-04 14:14:42 +0400

    omni

---
+++ src/main/java/org/traccar/protocol/OmniProtocol.java
@@ -0,0 +1,96 @@
+package org.traccar.protocol;
+
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.traccar.BaseProtocol;
+import org.traccar.CharacterDelimiterFrameDecoder;
+import org.traccar.PipelineBuilder;
+import org.traccar.TrackerServer;
+import org.traccar.config.Config;
+import org.traccar.model.Command;
+
+import jakarta.inject.Inject;
+
+public class OmniProtocol extends BaseProtocol {
+
+    @Inject
+    public OmniProtocol(Config config) {
+        setSupportedDataCommands(
+                Command.TYPE_CUSTOM,
+                Command.TYPE_IDENTIFICATION,
+                Command.TYPE_POSITION_SINGLE,
+                Command.TYPE_POSITION_PERIODIC,
+                Command.TYPE_POSITION_STOP,
+                Command.TYPE_ENGINE_STOP,
+                Command.TYPE_ENGINE_RESUME,
+                Command.TYPE_ALARM_ARM,
+                Command.TYPE_ALARM_DISARM,
+                Command.TYPE_REBOOT_DEVICE,
+                Command.TYPE_CONFIGURATION,
+                Command.TYPE_OUTPUT_CONTROL,
+                Command.TYPE_SET_INDICATOR,
+                Command.TYPE_GET_VERSION,
+                Command.TYPE_FIRMWARE_UPDATE,
+                Command.TYPE_SET_SPEED_LIMIT,
+                "unlock",
+                "lock",
+                "updateVehicleInfo",
+                "updateGeoInfo",
+                "updateLockInfo",
+                "engineOn",
+                "engineOff",
+                "modeEco",
+                "modeNormal",
+                "modeSport",
+                "headlightOn",
+                "headlightOff",
+                "taillightFlashingOn",
+                "taillightFlashingOff",
+                "inchSpeedDisplayOn",
+                "inchSpeedDisplayOff",
+                "cruiseOn",
+                "cruiseOff",
+                "blockChangeSpeedModeOn",
+                "blockChangeSpeedModeOff",
+                "headlightControlOn",
+                "headlightControlOff",
+                "speedLimitEco",
+                "speedLimitNormal",
+                "speedLimitSport",
+                "lookForOn",
+                "buzzerControlOn",
+                "set30MinGeoIntervalLocked",
+                "setGeoIntervalLocked",
+                "setGeoInterval5SecUnlocked",
+                "setGeoIntervalUnlocked",
+                "unlockBatteryLock",
+                "lockBatteryLock",
+                "unlockWheelLock",
+                "lockWheelLock",
+                "unlockCableLock",
+                "lockCableLock",
+                "turnOffLed",
+                "setLedRed",
+                "setLedGreen",
+                "defaultHeartbeatInterval",
+                "unlockHelmetLock",
+                "lockHelmetLock",
+                "getHelmetLockStatus",
+                "updateEcuFirmware",
+                "updateDisplayFirmware",
+                "updateIotFirmware",
+                "updateBms1Firmware",
+                "disableUnlockStatusUploadInformation");
+        addServer(new TrackerServer(config, getName(), false) {
+            @Override
+            protected void addProtocolHandlers(PipelineBuilder pipeline, Config config) {
+                pipeline.addLast(new CharacterDelimiterFrameDecoder(4 * 1024, false, "#", "\n"));
+                pipeline.addLast(new StringEncoder());
+                pipeline.addLast(new StringDecoder());
+                pipeline.addLast(new OmniProtocolEncoder(OmniProtocol.this));
+                pipeline.addLast(new OmniProtocolDecoder(OmniProtocol.this));
+            }
+        });
+    }
+
+}
